2651d601f471317f36cd784ee0b4d159ccedf824
Aleksandr Koklin <akoklin@2reallife.com>
2016-04-18 17:42:36 +0300

    added support to skip missing field

--- etcd/docoder_test.go
+++ etcd/docoder_test.go
@@ -225,3 +225,20 @@ func TestDecodeIgnoreTag(t *testing.T) {
 	assert.Equal(t, int64(10), s.Field1)
 	assert.Equal(t, int64(0), s.Field2)
 }
+
+func TestDecodeSkipMissingError(t *testing.T) {
+	etcd := new(test.KeysAPIMock)
+	etcd.On("Get", mock.AnythingOfType("*context.emptyCtx"), "/path/to/some/struct/Field", mock.AnythingOfType("*client.GetOptions")).Return(nil, client.Error{Code: client.ErrorCodeKeyNotFound, Message: "Key not found"})
+	etcd.On("Get", mock.AnythingOfType("*context.emptyCtx"), "/path/to/some/struct/Field2", mock.AnythingOfType("*client.GetOptions")).Return(&client.Response{Node: &client.Node{Value: "10"}}, nil)
+
+	var s = struct {
+		Field  int64
+		Field2 int64
+	}{}
+
+	decoder := NewDecoder(etcd)
+	decoder.SkipMissing(true)
+	err := decoder.Decode("/path/to/some/struct", &s)
+	assert.Nil(t, err)
+	assert.Equal(t, s.Field2, int64(10))
+}
